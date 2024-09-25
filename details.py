22c64a9221d8e87a63b2bbb9a0afefaa15b66b5d
𝕯𝖗𝖆𝖌𝖔𝖓🐉 <dragonex7778@gmail.com>
2024-09-25 13:23:36 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 24515547
 api_hash = "0809699a061f02fbbcc1a6c4443547ee"
-bot_token = "7471507395:AAEsy8BQrIM2wr2VqBU16xYPVTBdNpGJ3Bg"
+bot_token = "7645073824:AAFyfsGNTVvaMH1fTqpj2yz1-auyLCBt3TU"
 
